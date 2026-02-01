0876922237867dfd82059d147614cbe2ae749148
YangHongming <2097627409@qq.com>
2026-02-01 17:44:46 +0800

    修改token.rs

--- src/app/model/state/token.rs
+++ src/app/model/state/token.rs
@@ -91,7 +91,7 @@ impl TokenManager {
             alias = Cow::Owned(generate_unnamed_alias(id));
         }
 
-        if self.alias_map.contains_key(alias.as_ref()) {
+        if self.alias_map.contains_key(&*alias) {
             return Err(TokenError::AliasExists);
         }
 
@@ -173,7 +173,7 @@ impl TokenManager {
         if alias == UNNAMED || alias.starts_with(UNNAMED_PATTERN) {
             alias = Cow::Owned(generate_unnamed_alias(id));
         }
-        if self.alias_map.contains_key(alias.as_ref()) {
+        if self.alias_map.contains_key(&*alias) {
             return Err(TokenError::AliasExists);
         }
 
